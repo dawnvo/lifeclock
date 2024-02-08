ac3a237e61979bc8360ce9a13c00c35add919e84
downvo <dawnvora@gmail.com>
2024-02-08 09:30:31 +0900

    feat: use daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
+<script>
+  import "tailwindcss/tailwind.css";
+</script>
+
 <svelte:head>
   <title>인생시계</title>
   <meta name="description" content="당신은 지금 몇 시인가요?" />
 </svelte:head>
+
 <slot />
